54902b3e02ff5a92c589763f9decce581a13f879
Tomen1980 <agungjumantoroandrian@gmail.com>
2024-08-05 16:15:21 +0700

    +Fixing action list blok

--- resources/views/ListBlok.blade.php
+++ resources/views/ListBlok.blade.php
@@ -10,12 +10,12 @@
                 <img src='img/clipboard-text.png' alt=''class='w-[34px] h-[30px]'>
                 <p class='font-Fredoka font-medium text-2xl ml-2 mt-1 text-[#A9A9A9]'>List Blok</p>
             </div>
-            <ul class='ml-5 mt-5'>
+            <ul class='ml-5 mt-5 '>
                 @foreach ($Blok as $item)
-                    <a href="/monitoring/ip/{{$item->id}}" class='flex ml-5 mt-2 h-9 items-center  hover:bg-[#E5EBEE] hover:rounded-md hover:w-[90%]'>
-                        <img src='img/bus.png' alt=''class='w-[38px] h-[35px]'>
-                        <p class='font-Fredoka  text-2xl ml-2 mt-1 text-[#3C8DBC] font-medium'>{{ $item->namaBlok }}</p>
-                        <div class=' flex absolute right-16 space-x-1 mt-4'>
+                    {{-- <a href="/monitoring/ip/{{$item->id}}" class='flex border-red-500 border-2 ml-5 mt-2 h-9 items-center  hover:bg-[#E5EBEE] hover:rounded-md hover:w-[90%]'>
+                        <img src='img/bus.png' alt=''class='w-[38px] h-[35px] border-red-500 border-2'>
+                        <p class='font-Fredoka  text-2xl ml-2 mt-1 text-[#3C8DBC] font-medium border-red-500 border-2'>{{ $item->namaBlok }}</p>
+                        <div class=' flex absolute right-16 space-x-1 mt-4 '>
                             <a href="/blok/update/{{ $item->id }}">
                                 <button class=' items-center'>
                                     <img src='img/edit.png' alt=''class='w-[20px] h-[20px] '>
@@ -29,7 +29,28 @@
                                 </button>   
                             </form>
                         </div>
-                    </a>
+                    </a> --}}
+
+                    <div class="flex  mx-auto mt-2 h-9 items-center justify-between">
+                        <div class=" flex items-center justify-center">
+                            <img src='img/bus.png' alt=''class='w-[38px] h-[35px] '>
+                            <a href="/monitoring/ip/{{$item->id}}" class="font-Fredoka  text-2xl ml-2 mt-1 text-[#3C8DBC] font-medium">{{ $item->namaBlok }}</a>
+                        </div>
+                        <div class="flex space-x-1  justify-center items-center mr-10">
+
+                                <a href="/blok/update/{{ $item->id }}" class="w-[20px] h-[20px] ">
+                                    <img src='/img/edit.png' alt=''class='object-cover w-full h-full'>
+                                </a>
+                                <div class="w-[20px] h-[20px] ">
+                                    <form action="/blok/delete/{{ $item->id }}" method="POST">
+                                        <button>    
+                                            <img src='/img/sampah.png' alt=''class='object-cover w-full h-full'>
+                                        </button>
+                                    </form>
+                                </div>
+
+                        </div>
+                    </div>
                 @endforeach
                 
             </ul>
